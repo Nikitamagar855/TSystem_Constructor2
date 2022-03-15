7bc121aa7cad9b605b34792b674a837e4a05a5f5
Nikitamagar855 <89832430+Nikitamagar855@users.noreply.github.com>
2022-03-15 11:11:15 +0530

    Add project files.

---
+++ TSystem_Constructor2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSystem_Constructor2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("NIKITA");
+            Console.ReadLine();
+
+        }
+    }
+}
